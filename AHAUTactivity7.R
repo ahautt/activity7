be10c370553931357d179c9ce54e0a3a07df56e8
Ainsley Haut <ahaut@hamilton.edu>
2026-04-16 14:34:25 +0000

    description of changes

---
+++ AHAUTactivity7.R
@@ -0,0 +1,37 @@
+
+library(dplyr)
+library(ggplot2)
+library(olsrr)
+library(PerformanceAnalytics)
+
+# read in greenhouse gas data from reservoirs
+ghg <- read.csv("/cloud/project/activity7/Deemer_GHG_Data.csv")
+
+
+# log transform methane fluxes
+ghg$log.ch4 <- log(ghg$ch4+1)
+ghg$log.age <- log(ghg$age)
+ghg$log.DIP <- log(ghg$DIP+1)
+ghg$log.precip <- log(ghg$precipitation)
+
+#look at names
+unique(ghg$Region)
+
+# binary variable for boreal region
+ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
+# binary variable for tropical region
+ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
+# binary variable for alpine region
+ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
+# binary variable for known hydropower
+ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
+
+# multiple regression
+# creates a model object
+mod.full <- lm(log.ch4 ~ airTemp+
+                 surface.area+
+                 log.age+mean.depth+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
+
+mod.full
