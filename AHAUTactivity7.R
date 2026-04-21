e4f7c0b25b97ba43793a5c7227454696b73d20a0
Ainsley Haut <ahaut@hamilton.edu>
2026-04-21 22:23:11 +0000

    homework start

--- AHAUTactivity7.R
+++ AHAUTactivity7.R
@@ -13,6 +13,8 @@ ghg$log.ch4 <- log(ghg$ch4+1)
 ghg$log.age <- log(ghg$age)
 ghg$log.DIP <- log(ghg$DIP+1)
 ghg$log.precip <- log(ghg$precipitation)
+ghg$log.sa <- log(ghg$surface.area)
+ghg$log.runoff <- log(ghg$runoff+1)
 
 #look at names
 unique(ghg$Region)
@@ -26,12 +28,140 @@ ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
 # binary variable for known hydropower
 ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
 
+
 # multiple regression
 # creates a model object
+#ACTIVITY SEVEN PT 1 QUESTION 3
 mod.full <- lm(log.ch4 ~ airTemp+
-                 surface.area+
                  log.age+mean.depth+
                  log.DIP+
                  log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
 
-mod.full
+summary(mod.full)
+
+mod.edit <- lm(log.ch4 ~ airTemp+
+                 log.age+
+                 mean.depth+
+                 log.runoff+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
+
+summary(mod.edit)
+
+res.edit <- rstandard(mod.edit)
+fit.edit <- fitted.values(mod.edit)
+
+# qq plot
+qqnorm(res.edit, pch=19, col="grey50")
+qqline(res.edit)
+
+# shapiro-wilks test
+shapiro.test(res.edit)
+
+
+plot(fit.edit,res.edit, pch=19, col="grey50")
+abline(h=0)
+
+# isolate continuous model variables into data frame:
+
+reg.data <- data.frame(ghg$airTemp,
+                       ghg$log.age,ghg$mean.depth,
+                       ghg$log.DIP,
+                       ghg$log.precip)
+
+# make a correlation matrix 
+chart.Correlation(reg.data, histogram=TRUE, pch=19)
+
+summary(mod.edit)$coefficients
+table_out <- summary(mod.edit)$coefficients 
+
+# run stepwise
+full.step <- ols_step_forward_aic(mod.edit)
+# view table
+full.step 
+# check full model
+full.step$model
+# plot AIC over time
+plot(full.step )
+
+
+# IN CLASS PROMPT 2
+ETdat <- read.csv("/cloud/project/activity7/ETdata.csv")
+unique(ETdat$crop)
+
+# average fields for each month for almonds
+almond <- ETdat %>% # ET data
+  filter(crop == "Almonds") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE)) # average fields
+
+# visualize the data
+ggplot(almond, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
+
+# almond ET time series
+almond_ts <- ts(almond$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
+
+almondTrend <- almond_dec$trend
+almondSeason <- almond_dec$seasonal
+
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+pacf.plot <- pacf(na.omit(almond_ts))
+
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model1
+
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
+
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  # Plotting original data
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
